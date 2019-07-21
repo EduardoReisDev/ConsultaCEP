a1f9af46b732150ca385a20a9c8329375b2d3c65
EduardoReisDev <52722526+EduardoReisDev@users.noreply.github.com>
2019-07-21 17:29:53 -0300

    Final

---
+++ AppConsultaCep/AppConsultaCep/AppConsultaCep.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "afbea75a-927e-4198-8800-900986bba6c3",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
